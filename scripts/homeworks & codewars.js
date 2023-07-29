ee30c0a18045d2d9d40f274c547e80d88a6dec5b
ValeriyVinn <valeriy.all@gmail.com>
2023-07-29 20:11:11 +0300

    Added scroll-button with progress to H&C

--- scripts/homeworks & codewars.js
+++ scripts/homeworks & codewars.js
@@ -2,7 +2,7 @@ const acc = document.getElementsByClassName("accordion");
 let i;
 
 for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
+  acc[i].addEventListener("click", function () {
     this.classList.toggle("active");
     const panel = this.nextElementSibling;
     if (panel.style.maxHeight) {
@@ -12,3 +12,33 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 }
+
+// ============= Scroll btn =================
+
+let calcScrollValue = () => {
+  let scrollProgress = document.getElementById("progress");
+  let progressValue = document.getElementById("progress-value");
+  let pos = document.documentElement.scrollTop;
+  // console.log(pos)
+  let calcHeight =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  // console.log(calcHeight)
+  let scrollValue = Math.round((pos * 100) / calcHeight);
+  // console.log(scrollValue)
+  if (pos > 100) {
+    scrollProgress.style.display = "grid";
+  } else {
+    scrollProgress.style.display = "none";
+  }
+  scrollProgress.addEventListener("click", () => {
+    document.documentElement.scrollTop = 0;
+  });
+
+  scrollProgress.style.background = `conic-gradient(cornflowerblue ${scrollValue}%, #d7d7d7 ${scrollValue}%)`;
+};
+
+window.onscroll = calcScrollValue;
+window.onload = calcScrollValue;
+
+// ============= Scroll btn =================
